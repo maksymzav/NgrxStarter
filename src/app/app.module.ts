4a8a34298eae91d39dfc4687cea9708bc16868cb
maksymzav <maksym.zavorotynskyi@shopping-feed.com>
2024-07-27 11:09:53 -0700

    initialize material usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppComponent} from './app.component';
 import {StoreModule} from "@ngrx/store";
 import {EffectsModule} from "@ngrx/effects";
 import {HomeModule} from "./home/home.module";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   imports: [
@@ -19,6 +20,9 @@ import {HomeModule} from "./home/home.module";
   ],
   bootstrap: [
     AppComponent
+  ],
+  providers: [
+    provideAnimationsAsync()
   ]
 })
 export class AppModule { }
